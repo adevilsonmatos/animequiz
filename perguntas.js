a9b5b20aee80a5b2f6f08e6957e4fdac56c8b88e
adevilsonmatos <151065540+adevilsonmatos@users.noreply.github.com>
2024-12-09 10:40:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Naruto',
+    'Qual foi o primeiro professor do Naruto?',
+    'É o professor/sensei Iruka Umino que apareceu nos primeiros episódios do anime'
+)
+
+criaCartao(
+    'Dragon ball Z',
+    'Qual é a origem do nome Goku?',
+    'A origem de de seu nome vem de cenoura/carrot por conta que seu nome de nasçenca é kakarot mas também go pode ser traduzido para 5 e ku poode ser traduzido para 9 que juntando fica goku'
+)
+
+criaCartao(
+    'Digimon',
+    'Quem é o criador de digimon?',
+    'A criação de Digimon muitas vezes é atribuída a Akiyoshi Hongo, Na verdade, essa pessoa não existe poís na verdade esse nome é a combinação dos nome de todas as pessoas que criaram esse universo'
+)
+
+criaCartao(
+    'Pokémon',
+    'Pokémon tem quantos mascotes?',
+    'Ao todo são 2 mascotes sendo obviamente o Pikachu e o pokémon raposa Eevee'
+)
+
+criaCartao(
+     'Dan Dan',
+    'Qual é o nome verdadeiro nome do Okarun',
+    'Seu nome verdadeiro nome é Ken takakura'
+)
+
+criaCartao(
+    'One piece',
+    'Qual é o nome de todas as três espadas do Zoro',
+    'o nome delas são Wado Ichimonji, Sandai Kitetsu e Emma'
+)
